66a333493f8e3bac0a3b83b032e10d247fd8697f
Henry Cortez <henrycortez2509@gmail.com>
2021-11-25 17:03:32 -0600

    version 1.0.2-RELEASE

---
+++ src/main/java/com/siman/bolsaempleo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.siman.bolsaempleo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class HelloController {
+
+	@GetMapping("/saludar")
+	public String saludar() {
+		return "Hola Mundo";
+	}
+}
